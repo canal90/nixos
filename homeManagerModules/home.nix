c8bc2a6018d881753504f1fdfda0298c92e57ffd
G. Arends <gd@wmc.io>
2024-04-24 08:44:55 -0400

    Add stereo tool config

--- homeManagerModules/home.nix
+++ homeManagerModules/home.nix
@@ -9,7 +9,7 @@
 
   home.file."stereo_tool.qpwgraph".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.homeDirectory}/nixos/dotfiles/stereo_tool.qpwgraph";
   home.file.".buttrc".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.homeDirectory}/nixos/dotfiles/.buttrc";
-
+  home.file.".stereo_tool.rc".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.homeDirectory}/nixos/dotfiles/.stereo_tool.rc";
 
 
   home.stateVersion = "23.11";
